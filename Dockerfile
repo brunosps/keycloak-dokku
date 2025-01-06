65990f330ae736b2afa2b5a9171c116a5606315c
Bruno Santos <bruno@neometa.com.br>
2025-01-06 14:30:44 -0300

    Alterado para versão 25 do keycloak

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM quay.io/keycloak/keycloak:latest
+FROM quay.io/keycloak/keycloak:25.0
 
 WORKDIR /opt/keycloak
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -25,7 +25,7 @@ fi
 # To adjust log level, add:
 # -Dlogging.level.org.keycloak=DEBUG
 exec /opt/keycloak/bin/kc.sh start \
-        --proxy-headers forwarded --http-enabled true \
+        --proxy=edge \
         --http-port=$KEYCLOAK_HTTP_PORT \
         --hostname=$KEYCLOAK_HOSTNAME \
         --db=postgres \
