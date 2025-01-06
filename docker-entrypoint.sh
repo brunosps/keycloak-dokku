48df098434020ce785e87b9f9bee410a5d4582da
Bruno Santos <bruno@neometa.com.br>
2025-01-06 16:17:38 -0300

    Update Keycloak configuration for version 26.0 - fix proxy settings

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,18 +7,11 @@ export KC_DB_USERNAME="${KC_DB_USERNAME:-postgres}"
 export KC_DB_PASSWORD="${KC_DB_PASSWORD}"
 export KC_HOSTNAME="${KC_HOSTNAME:-loginunico.neometa.com.br}"
 export KC_HTTP_PORT="${KC_HTTP_PORT:-8080}"
-export KC_PROXY="${KC_PROXY:-edge}"
-export KC_HEALTH_ENABLED="${KC_HEALTH_ENABLED:-true}"
-export KC_METRICS_ENABLED="${KC_METRICS_ENABLED:-true}"
 
 # Iniciar o Keycloak
 exec /opt/keycloak/bin/kc.sh start \
-    --optimized \
     --http-enabled=true \
     --http-port=$KC_HTTP_PORT \
     --hostname=$KC_HOSTNAME \
-    --proxy-headers=forwarded \
-    --proxy-address-forwarding=true \
-    --health-enabled=$KC_HEALTH_ENABLED \
-    --metrics-enabled=$KC_METRICS_ENABLED
+    --proxy-headers=forwarded
 
